6a2ab797ec336ecc5cc6b7ab57ff39356b327b6c
Sai Kaushik Sadu <91894475+SaiKaushikSadu@users.noreply.github.com>
2024-01-31 12:50:34 +0530

    update the profile info commit

--- backend/app.py
+++ backend/app.py
@@ -97,6 +97,55 @@ def login():
             'status':'User Not Found'
         })
 
+# SignUp Update [Done]
+@app.route("/<id>/resignup",methods=['POST'])
+def resignup(id): 
+    body=request.json
+    age=body.get('age')
+    gender=body.get('gender')
+    city=body.get('city')
+    language=body.get('language')
+    height=body.get('height')
+    weight=body.get('weight')
+    diet_resctriction=body.get('diet_resctriction')
+    diet_pref=body.get('diet_pref')
+    religion=body.get('religion')
+    allergy=body.get('allergy')
+    severity=body.get('severity')
+    fav_cuisine=body.get('fav_cuisine')
+    cooking_skills=body.get('cooking_skills')
+    notifications=body.get('notifications')
+
+    print(age)
+    try:
+        db['user'].update_one(
+                {'_id': ObjectId(id)},
+                {
+                    '$set': {
+                        "age": age,
+                        "gender": gender,
+                        "city": city,
+                        "language": language,
+                        "height": height,
+                        "weight": weight,
+                        "diet_resctriction": diet_resctriction,
+                        "diet_pref": diet_pref,
+                        "religion": religion,
+                        "allergy": allergy,
+                        "severity": severity,
+                        "fav_cuisine": fav_cuisine,
+                        "cooking_skills": cooking_skills,
+                        "notifications": notifications
+                    }
+                }
+            )
+        return jsonify({
+            'status':'Success'
+        })
+    except:
+        return jsonify({
+            'status':'Error occured'
+        })
 
 if __name__ == '__main__':
     app.run(port=3001)
